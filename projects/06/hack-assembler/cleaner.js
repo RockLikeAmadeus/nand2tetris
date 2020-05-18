ca09a6edb5c4c0a1907d3fa185139e39307938eb
Alec <alec.hampton@charter.com>
2020-05-18 16:50:02 -0600

    Moved program cleaner into its own module

---
+++ projects/06/hack-assembler/cleaner.js
@@ -0,0 +1,36 @@
+// Takes a .asm file as an array of strings (separated by carriage returns) and
+// returns an identical array of strings but with all white space and comments
+// removed
+
+"use strict";
+
+module.exports = function(fileAsArray) {
+
+    let prog = [];
+
+    for (let i = 0; i < fileAsArray.length; i++) {
+
+        // Remove spaces
+        let line = fileAsArray[i].replace(/\s/g, '');
+
+        // Skip blank lines
+        if (line.length == 0) {
+            continue;
+        }
+
+        // Remove comments
+        let commentIndex = line.indexOf('//');
+        switch(commentIndex) {
+            case 0:
+                break;
+            case -1:
+                prog.push(line);
+                break;
+            default:
+                prog.push(line.substring(0, line.indexOf('//')));
+                break;
+        }
+    }
+
+    return prog;
+}
--- projects/06/hack-assembler/index.js
+++ projects/06/hack-assembler/index.js
@@ -1,34 +1,14 @@
 "use strict";
 
 const fs = require('fs');
+const cleaner = require('./cleaner');
 
+// Read the file in
 let file = fs.readFileSync(process.argv[2], 'utf8');
+// Convert it into an array
 let progRaw = file.split('\n');
-let prog = [];
-
-for (let i = 0; i < progRaw.length; i++) {
-
-    // Remove spaces
-    let line = progRaw[i].replace(/\s/g, '');
-
-    // Skip blank lines
-    if (line.length == 0) {
-        continue;
-    }
-
-    // Remove comments
-    let commentIndex = line.indexOf('//');
-    switch(commentIndex) {
-        case 0:
-            break;
-        case -1:
-            prog.push(line);
-            break;
-        default:
-            prog.push(line.substring(0, line.indexOf('//')));
-            break;
-    }
-}
+// Clean white space and comments from the program
+let prog = cleaner(progRaw);
 
 for (let i = 0; i < prog.length; i++) {
 
