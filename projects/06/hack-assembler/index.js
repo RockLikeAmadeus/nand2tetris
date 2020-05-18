1930c233261336f7a45f37d83b73615d7962971e
Alec <alec.hampton@charter.com>
2020-05-18 17:16:49 -0600

    Completed translation of A-instructions

--- projects/06/hack-assembler/index.js
+++ projects/06/hack-assembler/index.js
@@ -13,6 +13,19 @@ let prog = cleaner(progRaw);
 
 
 
+
+
+for (let i = 0; i < prog.length; i++) {
+
+    let line = prog[i];
+    console.log(`${i}\t${line}`);
+}
+
+console.log('');
+console.log('////////////////////////');
+console.log('');
+
+
 prog = translate(prog);
 
 for (let i = 0; i < prog.length; i++) {
--- projects/06/hack-assembler/translator.js
+++ projects/06/hack-assembler/translator.js
@@ -7,7 +7,39 @@ module.exports = function(prog) {
 
     let result = [];
 
+    for (let i = 0; i < prog.length; i++) {
 
+        let line = prog[i];
+
+        if (line[0] == '@') {
+            result.push(translateAInstruction(line));
+        }
+        else {
+            result.push(translateCInstruction(line));
+        }
+
+    }
 
     return result;
+}
+
+function translateAInstruction(ins) {
+    // Prefix:
+    let machineInstruction = '0';
+
+    // Convert value to binary
+    let numString = ins.substring(1, ins.length);
+    let binString = (+numString).toString(2); 
+    // Pad zeros
+    let binStringFull = `${'0'.repeat(15 - binString.length)}${binString}`;
+
+    machineInstruction += binStringFull;
+
+    return machineInstruction;
+}
+
+function translateCInstruction(ins) {
+    // Prefix:
+    let machineInstruction = '111';
+    return machineInstruction;
 }
