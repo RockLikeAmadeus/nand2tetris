617289f093a8742b4744e900f250d736a046c9ee
Alec <alec.hampton@charter.com>
2020-05-22 01:23:30 -0600

    start on translation

--- projects/07/vm-translator/index.js
+++ projects/07/vm-translator/index.js
@@ -11,6 +11,60 @@ console.log('Original program:\n');
 printProgram(progRaw);
 printSeparator();
 
+// Clean white space and comments from the program
+let prog = clean(progRaw);
+
+console.log('Without white space or comments:\n');
+printProgram(prog);
+printSeparator();
+
+// Translate VM Code
+translate(prog);
+
+function translate(prog) {
+
+    let result = [];
+
+    // For each line in the file
+    for (let i = 0; i < prog.length; i++) {
+
+        const line = prog[i];
+        result.push(`// ${line}`)
+
+        const terms = line.split(' ');
+
+        const command = terms[0];
+
+        if (terms.length == 3) {
+
+            const segment = terms[1];
+            const argument = terms[2];
+
+            // Memory access command
+            switch(command) {
+                case 'push':
+                    console.log('push!');
+                    break;
+        }
+        } else if (terms.length == 1) {
+            // Arithmetic / Logic command
+        }
+
+        
+
+    }
+
+    printProgram(result);
+
+}
+
+
+
+
+
+
+
+
 ///////////////////////////////////////////////////////////////////////////////
 
 function printSeparator() {
@@ -24,10 +78,3 @@ function printProgram(prog) {
         console.log(`${i}\t${line}`);
     }
 }
-
-// Clean white space and comments from the program
-let prog = clean(progRaw);
-
-console.log('Without white space or comments:\n');
-printProgram(prog);
-printSeparator();
