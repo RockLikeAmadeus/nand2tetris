753b23e51f0328430cbbb8c6fba0bb1e91583c55
Alec <alec.hampton@charter.com>
2020-05-26 22:12:06 -0600

    Added add command, not tested yet

--- projects/07/vm-translator/translator.js
+++ projects/07/vm-translator/translator.js
@@ -14,12 +14,12 @@ module.exports = function translate(prog) {
 
         const terms = line.split(' ');
 
-        const command = terms[0];
+        const command = terms[0].trim();
 
         if (terms.length == 3) {
 
-            const segment = terms[1];
-            const argument = terms[2];
+            const segment = terms[1].trim();
+            const argument = terms[2].trim();
 
             // Memory access command
             switch(command) {
@@ -37,14 +37,15 @@ module.exports = function translate(prog) {
 
         } else if (terms.length == 1) {
             // Arithmetic / Logic command
+            switch(command) {
+                case 'add':
+                    result = result.concat(add());
+                    break;
+            }
         }
-
-        
-
     }
 
     printer.printProgram(result);
-
 }
 
 // Returns an array containing the hack instructions necessary
@@ -54,6 +55,13 @@ function pushValue(val) {
     // D = val
     hackInstructions.push(`@${val}`);
     hackInstructions.push('D=A');
+    // *SP = D; SP++;
+    hackInstructions = hackInstructions.concat(pushDRegister());
+    return hackInstructions;
+}
+
+function pushDRegister() {
+    let hackInstructions = [];
     // *SP = D
     hackInstructions.push('@SP');
     hackInstructions.push('A=M');
@@ -61,5 +69,28 @@ function pushValue(val) {
     // SP++
     hackInstructions.push('@SP');
     hackInstructions.push('M=M+1');
+    return hackInstructions;
+}
+
+// Returns an array containing the hack instructions necessary
+// to add the top two values in the stack
+function add() {
+    let hackInstructions = [];
+    // SP--
+    hackInstructions.push('@SP');
+    hackInstructions.push('M=M-1');
+    // D = *SP
+    hackInstructions.push('@SP');
+    hackInstructions.push('A=M');
+    hackInstructions.push('D=M');
+    // SP--
+    hackInstructions.push('@SP');
+    hackInstructions.push('M=M-1');
+    // D = D + *SP
+    hackInstructions.push('@SP');
+    hackInstructions.push('A=M');
+    hackInstructions.push('D=D+M');
+    // *SP = D; SP++;
+    hackInstructions = hackInstructions.concat(pushDRegister());
     return hackInstructions;
 }
